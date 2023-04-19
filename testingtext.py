1ece0ddb417809675787da71ee49ade625e7b8e1
BitaniaS <bitania.samuel95@gmail.com>
2023-04-19 13:53:58 -0400

    draft

--- testingtext.py
+++ testingtext.py
@@ -9,10 +9,52 @@ from keras.models import Model
 from keras.optimizers import Adam
 import random
 from keras.preprocessing.image import ImageDataGenerator
-
+from keras import regularizers
 from keras.callbacks import ModelCheckpoint
+from keras.callbacks import Callback
+from sklearn.metrics import accuracy_score, f1_score
 
 
+'''
+# calculating accurary and F1 for each layer 
+class LayerwiseAccuracyCallback(Callback):
+    def __init__(self, model, x, y):
+        super().__init__()
+        self.model = model
+        self.x = x
+        self.y = y
+    
+    def on_epoch_end(self, epoch, logs=None):
+        layer_accuracies = []
+        for layer in self.model.layers:
+            if hasattr(layer, 'output'):
+                y_pred = layer.predict(self.x)
+                y_pred = np.round(y_pred).astype(int)
+                layer_acc = accuracy_score(self.y, y_pred)
+                layer_accuracies.append(layer_acc)
+            else:
+                layer_accuracies.append(0)
+        logs['layer_accuracies'] = layer_accuracies
+
+class LayerwiseF1ScoreCallback(Callback):
+    def __init__(self, model, x, y):
+        super().__init__()
+        self.model = model
+        self.x = x
+        self.y = y
+    
+    def on_epoch_end(self, epoch, logs=None):
+        layer_f1_scores = []
+        for layer in self.model.layers:
+            if hasattr(layer, 'output'):
+                y_pred = layer.predict(self.x)
+                y_pred = np.round(y_pred).astype(int)
+                layer_f1 = f1_score(self.y, y_pred)
+                layer_f1_scores.append(layer_f1)
+            else:
+                layer_f1_scores.append(0)
+        logs['layer_f1_scores'] = layer_f1_scores
+'''
 
 
 '''
@@ -24,6 +66,9 @@ os.listdir : gets a list of all files and directories in the specified directory
 '''
 image_list=os.listdir('./PageSegData/PageImg')
 image_list=[filename.split(".")[0]for filename in image_list] #use os.path.splittext() as an alternate 
+
+
+
 
 
 '''
@@ -52,8 +97,8 @@ In this case, all the pixels are initially labeled as 0, indicating that they do
 #to convert the segmentation mask into one-hot encoded representation 
 def get_segmented_img(img,n_classes): # also known as binary segmentation mask 
 
-    seg_labels=np.zeros((512,512,1)) # creating a Numpy Array width * height*channels 
-    img=cv2.resize(img,(512,512)) #resize img to height and width of 512 (same case as the gray scale thingy, do I need to preprocess ?)
+    seg_labels=np.zeros((320,320,1)) # creating a Numpy Array width * height*channels 
+    img=cv2.resize(img,(320,320)) #resize img to height and width of 512 (same case as the gray scale thingy, do I need to preprocess ?)
     
     img=img[:,:,0] # i'm not sure if this line converts it to grey scale or not ( do I need to do the preprocessing part ? )
      # selecting only the first channel of the resized image array this effectively converts it to grayscale 
@@ -73,13 +118,20 @@ def get_segmented_img(img,n_classes): # also known as binary segmentation mask
     # cv2.destroyAllWindows()
     return seg_labels
 
-# imga = cv2.imread('./PageSegData/PageSeg/1_mask.png')
-# get_segmented_img(imga)
+# img=cv2.imread(f'./PageSegData/PageImg/229.JPG',0)
+# cv2.imshow('image',img)
+
+# img2=cv2.imread(f'./PageSegData/PageImg/229.JPG',0)
+# visualize(img,img2)
+# # cv2.imshow('image',img)
+# # imga = cv2.imread('./PageSegData/PageSeg/1_mask.png')
+# # get_segmented_img(imga)
 
 
 def preprocess_img(img):
-    img=cv2.resize(img,(512,512))
+    img=cv2.resize(img,(320,320))
     return img
+
 
 #function that creates a numpy array of input images (actual text images)
 # and output segmentation masks the output masks we have from the get_segmented_img function
@@ -103,9 +155,9 @@ def preprocess_img(img):
 #     yield np.array(X),np.array(Y)
 
 
-#batch generator 
+# batch generator 
 
-def batch_generator(filelist,n_classes,batch_size,augment=True):
+def batch_generator(filelist,n_classes,batch_size,augment):
   datagen = ImageDataGenerator(
       rotation_range=30,
       width_shift_range=0.1, #shifts the width of all the pixels of the image keza teg lay yalew to the beginning part yehedal
@@ -125,13 +177,13 @@ def batch_generator(filelist,n_classes,batch_size,augment=True):
     Y=[]
     for i in range(batch_size):
       fn=random.choice(filelist)
-      img=cv2.imread(f'/content/PageSegData/PageImg/{fn}.JPG',0)
+      img=cv2.imread(f'./PageSegData/PageImg/{fn}.JPG',0)
       ret,img=cv2.threshold(img,150,255,cv2.THRESH_BINARY_INV)
-      img=cv2.resize(img,(512,512))
+      img=cv2.resize(img,(320,320))
       img=np.expand_dims(img,axis=-1)
       img=img/255
 
-      seg=cv2.imread(f'/content/PageSegData/PageSeg/{fn}_mask.png',1)
+      seg=cv2.imread(f'./PageSegData/PageSeg/{fn}_mask.png',1)
       seg=get_segmented_img(seg,n_classes)
 
       if augment:
@@ -141,68 +193,25 @@ def batch_generator(filelist,n_classes,batch_size,augment=True):
 
       X.append(img)
       Y.append(seg)
-
     yield np.array(X),np.array(Y)
 
 
 
 
-
-# #data format is channels last in this case. 
-def unet(pretrained_weights = None,input_size = (512,512,1)):
-    inputs = Input(input_size)
-    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
-    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
-    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
-    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
-    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
-    drop4 = Dropout(0.5)(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
-
-    #decoder network mijemerebet
-    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
-    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
-    drop5 = Dropout(0.5)(conv5)
-
-    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
-    merge6 = concatenate([drop4,up6], axis = 3)
-    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
-    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
-
-    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
-    merge7 = concatenate([conv3,up7], axis = 3)
-    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
-    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
-
-    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
-    merge8 = concatenate([conv2,up8], axis = 3)
-    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
-    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
-
-    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
-    merge9 = concatenate([conv1,up9], axis = 3)
-    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
-    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
-    model = Model(inputs,conv10)
+metric =  ['accuracy','f1_score','precision','recall']
+input_size = (320,320,1)
 
 
-    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
-    
-    #model.summary()
-    if(pretrained_weights):
-        model.load_weights(pretrained_weights)
-    return model
-
-model=unet()
-model.summary() 
+params = {
+    'lr': [1e-3, 1e-4, 1e-5], 
+    'batch_size': [4,8,16],
+    'epochs': [10, 20, 30],
+    'filters': [8, 16, 32],
+    'steps':[100,150,200],
+    'w_decay' : [0.00001, 0.0001, 0.001],
+    'step' :[100,150,200],
+    'augment_v': ['True','False']
+}
 
 #splitting the dataset (lela type of splitting new metekem yalebign)
 random.shuffle(image_list)
@@ -210,15 +219,130 @@ file_train=image_list[0:int(0.70*len(image_list))]
 file_valid=image_list[int(0.70*len(image_list)): int(0.90*len(image_list))]
 file_test=image_list[int(0.90*len(image_list)):]
 
-#TRAINING THE MODEL
-# this saves the only the weigths of the model after every epoch 
-# I am not sure if i should also use the SAVE_BEST_ONLY parameter 
-mc = ModelCheckpoint('weights{epoch:08d}.h5', 
-                                     save_weights_only=True, period=1)
+# #data format is channels last in this case. 
+def unet(n_items):
 
-# is should change the steps_per_epoch 
-model.fit_generator(batch_generator(file_train,2,2),epochs=3,steps_per_epoch=1000,validation_data=batch_generator(file_test,2,2),
-                    validation_steps=400,callbacks=[mc],shuffle=1)
+    for i in range (n_items):
+        valid_gen = batch_generator(file_valid, batch_size=2, height=2, width=2)
+        # Randomly choose hyperparameters
+        learn_r = random.choice(params['lr'])
+        batch_s = random.choice(params['batch_size']) 
+        epochs = random.choice(params['epochs'])
+        filters = random.choice(params['filters'])
+        w_decay = random.choice(params['w_decay'])   
+        step = random.choice(params['step']) 
+        augment_v = random.choice(params['augment_v']) 
 
+
+        inputs = Input(input_size)
+        conv1 = Conv2D(filters, 3, activation='relu', padding='same')(inputs)
+        conv1 = Conv2D(filters, 3, activation='relu', padding='same')(conv1)
+        pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+        conv2 = Conv2D(filters, 3, activation='relu', padding='same')(pool1)
+        conv2 = Conv2D(filters, 3, activation='relu', padding='same')(conv2)
+        pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+        conv3 = Conv2D(filters, 3, activation='relu', padding='same')(pool2)
+        conv3 = Conv2D(filters, 3, activation='relu', padding='same')(conv3)
+        pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+        up4 = UpSampling2D(size=(2, 2))(pool3)
+        up4 = Conv2D(filters, 2, activation='relu', padding='same')(up4)
+        up4 = Concatenate()([up4, conv3])
+        up4 = Conv2D(filters, 3, activation='relu', padding='same')(up4)
+
+        up5 = UpSampling2D(size=(2, 2))(up4)
+        up5 = Conv2D(filters, 2, activation='relu', padding='same')(up5)
+        up5 = Concatenate()([up5, conv2])
+        up5 = Conv2D(filters, 3, activation='relu', padding='same')(up5)
+
+        up6 = UpSampling2D(size=(2, 2))(up5)
+        up6 = Conv2D(filters, 2, activation='relu', padding='same')(up6)
+        up6 = Concatenate()([up6, conv1])
+        up6 = Conv2D(filters, 3, activation='relu', padding='same')(up6)
+
+        outputs = Conv2D(filters, 1, activation='softmax')(up6)
+        model = Model(inputs=[inputs], outputs=[outputs])
+        
+        # the order of it mastekakel alebign
+        mc = ModelCheckpoint('best_weight_for{i}.h5', monitor=['f1_score','val_loss','precision','recall'],
+                             save_best_only=True, period=1)
+        model.compile(optimizer = Adam(lr = learn_r), loss = 'binary_crossentropy', metrics= metric,
+                       kernel_regularizer = regularizers.L2(w_decay),batch_regularizer= regularizers.L2(w_decay)  )
+     
+        csv_logger = csv_logger('training_history.csv',append = True)
+        history = model.fit(batch_generator(file_train,2,batch_s,augment_v),epochs=epochs,steps_per_epoch=step,validation_data=valid_gen,
+                   validation_steps=400,callbacks=[mc,csv_logger],shuffle=1)
+        val_loss = history.history['val_loss']
+        val_acc = history.history['accuracy']
+        f1= history.history['f1_score']
+        prec = history.history['precision']
+        rec = history.history['recall']
+        
+
+        # score = model.evaluate(valid_gen, metrics = metric ) #the hyperparameters mastekakel alebign
+
+    #model.summary()
+    # if(pretrained_weights):
+    #     model.load_weights(pretrained_weights)
+        return model
+
+model=unet()
+model.summary() 
+
+
+
+
+
+# #TRAINING THE MODEL
+# # this saves the only the weigths of the model after every epoch 
+# # I am not sure if i should also use the SAVE_BEST_ONLY parameter 
+# mc = ModelCheckpoint('weights{epoch:08d}.h5', 
+#                                      save_weights_only=True, period=1)
+# # callback_accuracy = LayerwiseAccuracyCallback(model, x_val, y_val)
+# # callback_f1_score = LayerwiseF1ScoreCallback(model, x_val, y_val)
+
+# # is should change the steps_per_epoch 
+# # model.fit_generator(batch_generator(file_train,2,2),epochs=3,steps_per_epoch=1000,validation_data=batch_generator(file_valid,2,2),
+# #                     validation_steps=400,callbacks=[mc],shuffle=1)
+
+
+
+
+# def random_search(params, n_iter=10):
+#     best_score = 0
+#     best_params = None
+#     for i in range(n_iter):
+#         # Randomly choose hyperparameters
+#         lr = random.choice(params['lr'])
+#         batch_size = random.choice(params['batch_size']) 
+#         epochs = random.choice(params['epochs'])
+#         filters = random.choice(params['filters'])
+        
+#         # Build model with chosen hyperparameters
+#         model = unet(lr=lr, batch_size=batch_size, epochs=epochs, filters=filters)
+        
+#         # Train model and get validation score
+        
+#         # metric =  ['accuracy','f1_macro','precision','recall']
+#         model.fit(batch_generator(file_train,2,batch_size = batch_size),epochs=3,steps_per_epoch=1000,validation_data=valid_gen,
+#                     validation_steps=400,callbacks=[mc],shuffle=1)
+        
+#         # model.fit_generator(train_gen, epochs=epochs, validation_data=val_gen)
+#         score = model.evaluate(valid_gen, metrics = metric )
+        
+#         # Save best model
+#         if score > best_score:
+#             best_score = score
+#             best_params = {
+#                 'lr': lr,
+#                 'batch_size': batch_size, 
+#                 'epochs': epochs,
+#                 'filters': filters
+#             }
+            
+#     print(f'Best score: {best_score}') 
+#     print(f'Best hyperparameters: {best_params}')
 
 
